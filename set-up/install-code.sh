3c826b0b66103d2ad0a576482cf03155bb784987
Jim Priestley <japriest@microsoft.com>
2018-07-26 16:02:17 -0400

    saving a copy of the code install

---
+++ set-up/install-code.sh
@@ -0,0 +1,11 @@
+#install the repo key and repo pointer
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+#update repos
+sudo apt-get update
+#install VS Code
+sudo apt-get install code
+
+#sudo apt-get install code-insiders
